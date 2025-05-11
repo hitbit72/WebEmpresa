c7b3f1f36f08434c6498eb36bc60be1753fae474
hitbit72 <hitbit2@hotmail.com>
2025-05-11 08:03:18 +0200

    first commit

---
+++ webempresa/local_settings.py
@@ -0,0 +1,11 @@
+"""
+user:   admin
+pass:   admin123456
+"""
+
+# Configuracion correo
+# Looking to send emails in production? Check out our Email API/SMTP product!
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_HOST_USER = '125c9508a92f79'
+EMAIL_HOST_PASSWORD = 'ca22558f293c2d'
+EMAIL_PORT = '2525'
--- webempresa/settings.py
+++ webempresa/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 
-user:   admin
-pass:   admin123456
 """
 
 from pathlib import Path
@@ -156,10 +154,15 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-
 # Configuracion correo
 # Looking to send emails in production? Check out our Email API/SMTP product!
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = '125c9508a92f79'
-EMAIL_HOST_PASSWORD = 'ca22558f293c2d'
-EMAIL_PORT = '2525'
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = ''
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
